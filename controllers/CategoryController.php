bc593617713a302106a24d22983bc1382ad0b9cf
张晓旭 <itmuzi@aliyun.com>
2018-09-25 17:22:14 +0800

    无限极分类

---
+++ controllers/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+namespace controllers;
+
+class CategoryController
+{
+    // 分类首页
+    public function index()
+    {
+        view('category/index');
+    }
+    //  创建分类
+    public function create()
+    {
+        view('category/create');
+    }
+    //  处理添加分类表单
+    public function add()
+    {
+
+    }
+    //   编辑分类
+    public function edit()
+    {
+        view('category/create');
+    }
+    //  处理编辑分类表单
+    public function modify()
+    {
+
+    }
+    //  删除分类
+    public function delete()
+    {
+        view('category/create');
+    }
+}
---
+++ libs/DB.php
@@ -0,0 +1,22 @@
+<?php
+namespace libs;
+
+class DB
+{
+    // 连接数据库
+    private static $_obj = null;
+    public $_pdo ;
+    private function __construct(){
+        $this->_pdo = new \PDO('mysql:host=127.0.0.1;dbname=jxshop','root','123456');
+        $this->_pdo->exec('set names utf8');
+    }
+    private function __clone() {}
+    public static function make()
+    {
+        if(self::$_obj == null)
+        {
+            self::$_obj = new self;
+        }
+        return self::$_obj;
+    }
+}
